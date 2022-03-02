140968f7337923cf6acce9016191c01cc9df4224
Iakov Lilo <jlilo@hwdtech.com>
2022-03-02 16:16:17 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyProxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+            var app = builder.Build();
+            app.MapReverseProxy();
+            app.Run();
+        }
+    }
+}
